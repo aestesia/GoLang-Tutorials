0e1b7efdb04f0bda56de1eae5016265f02c8ad7b
aestesia <timothyhudson23@gmail.com>
2025-07-07 21:34:50 +0700

    Variables Constants Data Types

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	// fmt.Println("Hello World")
+	var intNum int = 32767
+	fmt.Println(intNum)
+
+	var floatNum float64 = 12345678.9
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result = floatNum32 + float32(intNum32)
+	fmt.Println(result)
+
+	var intNum1 int = 3
+	var intNum2 int = 2
+	fmt.Println(intNum1 / intNum2)
+	fmt.Println(intNum1 % intNum2)
+
+	var myString string = "Hello World"
+	fmt.Println(myString)
+
+	fmt.Println(utf8.RuneCountInString("A"))
+
+	var myRune rune = 'a'
+	fmt.Println(myRune)
+
+	var myBoolean bool = false
+	fmt.Println(myBoolean)
+
+	//var myVar = "text"
+	myVar := "text"
+	fmt.Println(myVar)
+
+	var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "can't be changed"
+	fmt.Println(myConst)
+
 }
