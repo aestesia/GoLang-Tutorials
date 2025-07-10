46aef1c96560b0ad7dd037a450d8eddc35c399f9
aestesia <timothyhudson23@gmail.com>
2025-07-10 22:14:38 +0700

    String Runes and bytes

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -81,49 +81,61 @@ func main() {
 
 	// -------------------------------------------------------------------
 
-	//var intArr [3]int32 = [3]int32{1,2,3}
-	//intArr := [3]int32{1, 2, 3}
-	intArr := [...]int32{1, 2, 3}
-	// intArr[1] = 123
-	// fmt.Println(intArr[0])
-	// fmt.Println(intArr[1:3])
-	fmt.Println(intArr)
+	// //var intArr [3]int32 = [3]int32{1,2,3}
+	// //intArr := [3]int32{1, 2, 3}
+	// intArr := [...]int32{1, 2, 3}
+	// // intArr[1] = 123
+	// // fmt.Println(intArr[0])
+	// // fmt.Println(intArr[1:3])
+	// fmt.Println(intArr)
 
-	var intSlice []int32 = []int32{4, 5, 6}
-	fmt.Printf("The length is %v with capacity %v", len(intSlice), cap(intSlice))
-	intSlice = append(intSlice, 7)
-	fmt.Printf("\nThe length is %v with capacity %v", len(intSlice), cap(intSlice))
+	// var intSlice []int32 = []int32{4, 5, 6}
+	// fmt.Printf("The length is %v with capacity %v", len(intSlice), cap(intSlice))
+	// intSlice = append(intSlice, 7)
+	// fmt.Printf("\nThe length is %v with capacity %v", len(intSlice), cap(intSlice))
 
-	var intSlice2 []int32 = []int32{8, 9}
-	intSlice2 = append(intSlice, intSlice2...)
-	fmt.Println(intSlice)
+	// var intSlice2 []int32 = []int32{8, 9}
+	// intSlice2 = append(intSlice, intSlice2...)
+	// fmt.Println(intSlice)
 
-	var intSlice3 []int32 = make([]int32, 3, 10)
-	fmt.Println(intSlice3)
+	// var intSlice3 []int32 = make([]int32, 3, 10)
+	// fmt.Println(intSlice3)
 
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap)
+	// var myMap map[string]uint8 = make(map[string]uint8)
+	// fmt.Println(myMap)
 
-	var myMap2 = map[string]uint8{"Adam": 23, "Sarah": 45}
-	fmt.Println(myMap2["Adam"])
-	var age, ok = myMap2["Jason"]
-	if ok {
-		fmt.Println("The age is %v", age)
-	} else {
-		fmt.Println("invalid name")
+	// var myMap2 = map[string]uint8{"Adam": 23, "Sarah": 45}
+	// fmt.Println(myMap2["Adam"])
+	// var age, ok = myMap2["Jason"]
+	// if ok {
+	// 	fmt.Println("The age is %v", age)
+	// } else {
+	// 	fmt.Println("invalid name")
+	// }
+
+	// for name, age := range myMap2 {
+	// 	fmt.Printf("Name: %v Age: %v\n", name, age)
+	// }
+
+	// for i, v := range intArr {
+	// 	fmt.Printf("Index: %v, Value: %v \n", i, v)
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+	// -------------------------------------------------------------------
+
+	//var myString = "résumé"
+	var myString = []rune("résumé")
+	var indexed = myString[0]
+	fmt.Printf("%v, %T", indexed, indexed)
+	for i, v := range myString {
+		fmt.Println(i, v)
 	}
 
-	for name, age := range myMap2 {
-		fmt.Printf("Name: %v Age: %v\n", name, age)
-	}
-
-	for i, v := range intArr {
-		fmt.Printf("Index: %v, Value: %v \n", i, v)
-	}
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+	var myRune = 'a'
+	fmt.Printf("\nmyRune = %v", myRune)
 
 }
 
