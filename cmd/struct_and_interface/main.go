c95dc42b171fae3717f71566187e0af860812dba
aestesia <timothyhudson23@gmail.com>
2025-07-11 23:20:53 +0700

    interface

--- cmd/struct_and_interface/main.go
+++ cmd/struct_and_interface/main.go
@@ -12,14 +12,37 @@ type gasEngine struct {
 // type owner struct {
 // 	name string
 // }
+type electricEngine struct {
+	mpkwh uint8
+	kwh   uint8
+}
 
 func (e gasEngine) milesLeft() uint8 {
 	return e.gallons * e.mpg
 }
 
+func (e electricEngine) milesLeft() uint8 {
+	return e.kwh * e.mpkwh
+}
+
+type engine interface {
+	milesLeft() uint8
+}
+
+func canMakeIt(e engine, miles uint8) {
+	if miles <= e.milesLeft() {
+		fmt.Println("made it")
+	} else {
+		fmt.Println("fuel up")
+	}
+}
+
 func main() {
 	//var myEngine gasEngine = gasEngine{25, 15, owner{"John"}}
+
 	var myEngine gasEngine = gasEngine{25, 15}
+	//var myEngine electricEngine = electricEngine{25, 15}
+
 	//myEngine.mpg = 15
 	//fmt.Println(myEngine.mpg, myEngine.gallons, myEngine.ownerInfo.name)
 	//fmt.Println(myEngine.mpg, myEngine.gallons, myEngine.name)
@@ -31,6 +54,8 @@ func main() {
 	// }{25,15}
 
 	//fmt.Println(myEngine.mpg, myEngine.gallons)
+	canMakeIt(myEngine, 50)
+
 	fmt.Printf("Total miles left in a tank %v", myEngine.milesLeft())
 
 }
